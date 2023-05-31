98e97d9eb1af8fe3086a2a962b50566210c5c36c
kabornblack <shaibu.kabiru@email.com>
2023-05-31 22:32:10 +0300

    Connected successfully to a mongoDB atlass DB using mongoose

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 const app = require("./app");
 
+dotenv.config({path: "./config.env"});
 
-const port = 3000;
-app.listen(port, () => {
-    console.log(`App listening on port ${port}...`);
+const DB = process.env.DATABASE.replace("<PASSWORD>", process.env.DATABASE_PASSWORD);
+
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }).then(con => {
+    //con is decleared but never used
+    console.log("DB connection successfull!");
 });
 
+const port = process.env.port;
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}...`);
+
+});
