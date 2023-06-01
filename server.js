39125d42485a1a6224aad44334903473e5663487
kabornblack <shaibu.kabiru@email.com>
2023-06-01 22:45:56 +0300

    Successully created a tourSchema and model

--- server.js
+++ server.js
@@ -7,12 +7,32 @@ dotenv.config({path: "./config.env"});
 const DB = process.env.DATABASE.replace("<PASSWORD>", process.env.DATABASE_PASSWORD);
 
 mongoose.connect(DB, {
+    //to connect to local (process.env.DATABASE_LOCAL, {
     useNewUrlParser: true,
     useUnifiedTopology: true
   }).then(con => {
     //con is decleared but never used
     console.log("DB connection successfull!");
 });
+
+const tourSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "A tour must have a name"],
+        unique: true
+    },
+    rating: {
+        type: Number,
+        default: 4.5
+    },
+    price: {
+        type: Number,
+        required: [true, "A tour must have a price"]
+    } 
+});
+
+const Tour = mongoose.model("Tour", tourSchema);
+
 
 const port = process.env.port;
 app.listen(port, () => {
