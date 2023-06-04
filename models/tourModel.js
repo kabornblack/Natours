9425f58dc598c72c250cc6b8d2c0fff1b2723a86
kabornblack <shaibu.kabiru@email.com>
2023-06-04 23:48:29 +0300

    Modified the tourSchema to contain all the required data for the project

--- models/tourModel.js
+++ models/tourModel.js
@@ -4,18 +4,58 @@ const tourSchema = new mongoose.Schema({
     name: {
         type: String,
         required: [true, "A tour must have a name"],
-        unique: true
+        unique: true,
+        trim: true
     },
-    rating: {
+    duration: {
+        type: Number,
+        required: [true, "A tour must have a duration"]
+    },
+    maxGroupSize: {
+        type: Number,
+        required: [true, "A tour must have a group size"]
+    },
+    difficulty: {
+        type: String,
+        required: [true, "A tour must have difficulty"]
+    },
+    ratingsAverage: {
         type: Number,
         default: 4.5
+    },
+    ratingQuantity: {
+        type: Number,
+        default: 0
     },
     price: {
         type: Number,
         required: [true, "A tour must have a price"]
-    } 
+    },
+    priceDiscount: Number,
+    summary: {
+        type: String,
+        trim: true,
+        required: [true, "A tour must have a description"]
+    },
+    description: {
+        type: String,
+        trim: true
+    },
+    imageCover: {
+        //Image file will be read and saved in a string that will be used here
+        type: String,
+        required: [true, "A tour must have a cover image"]
+    },
+    images: [String],
+    //An array of strings
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    },
+    startDate: [Date]
+    //An array of dates
 });
 
 const Tour = mongoose.model("Tour", tourSchema);
 
-model.exports = Tour;
+module.exports = Tour;
