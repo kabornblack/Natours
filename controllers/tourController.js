29f828df017081a12179ae76294cbabcd81666b1
kabornblack <shaibu.kabiru@email.com>
2023-06-07 01:08:44 +0300

    Added fields to what the client can see or what they wish to see

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -49,6 +49,14 @@ exports.getAllTours = async (req, res) => {
         } else {
             query = query.sort("-createdAt");
         };
+
+        //4) FIELD LIMITING
+        if (req.query.fields) {
+            const fields = req.query.fields.split(",").join(" ");
+            query = query.select(fields);
+        } else {
+            query = query.select("-__v");
+        };
         
         //EXECUTE QUERY
         const tours = await query;
