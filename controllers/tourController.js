63e18062005390b9c381917dfc28fd9f36823758
kabornblack <shaibu.kabiru@email.com>
2023-06-07 00:46:38 +0300

    Add a sort and set a default to the -createdAt the recently created

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -37,11 +37,18 @@ exports.getAllTours = async (req, res) => {
         //2) ADVANCE FILTERING
         let queryStr = JSON.stringify(queryObj);
         queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
-        console.log(JSON.parse(queryStr));
 
-        const query = Tour.find(JSON.parse(queryStr));
+        let query = Tour.find(JSON.parse(queryStr));
 
         // const query = Tour.find().where("duration").equals(5).where("difficulty").equals("easy");
+
+        //3) SORTING
+        if (req.query.sort) {
+            const sortBy = req.query.sort.split(",").join(" ");
+            query = query.sort(sortBy);
+        } else {
+            query = query.sort("-createdAt");
+        };
         
         //EXECUTE QUERY
         const tours = await query;
