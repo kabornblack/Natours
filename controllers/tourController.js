7cca07d6dcb6883f69d4d4b9edf6727a91518659
kabornblack <shaibu.kabiru@email.com>
2023-06-08 00:06:53 +0300

    Refactored the API and make it as reusable as possible

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const Tour = require("./../models/tourModel");
+const APIFeatures = require("./../utils/apiFeatures");
 
 // const tours = JSON.parse(fs.readFileSync(`${__dirname}/../starter/dev-data/data/tours-simple.json`));
 
@@ -10,76 +11,11 @@ exports.aliasTopTours = (req, res, next) => {
     next();
 };
 
-// exports.checkID = (req, res, next, val) => {
-//     console.log(`The next value is ${val}`);
-//     if (req.params.id * 1 > tours.length) {
-//         return res.status(404).json({
-//             status: "fail",
-//             message: "Cant find ID"
-//         });
-//     }
-//     next();
-// };
-
-// Just leave here for ref
-
-// exports.checkBody = (req, res, next) => {
-//     if (!req.body.name || !req.body.price) {
-//         return res.status(400).json({
-//             status: "fail",
-//             message: "Missing name or price"
-//         });
-//     };
-//     next();
-// };
-
 exports.getAllTours = async (req, res) => {
     try {
-        //BUILD QUERY
-        //1) FILTERING
-        const queryObj = { ...req.query };
-        const deletedObjs = ["limit", "page", "sort", "fields"];
-        deletedObjs.forEach(el => delete queryObj[el]);
-
-        //2) ADVANCE FILTERING
-        let queryStr = JSON.stringify(queryObj);
-        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
-
-        let query = Tour.find(JSON.parse(queryStr));
-
-        // const query = Tour.find().where("duration").equals(5).where("difficulty").equals("easy");
-
-        //3) SORTING
-        if (req.query.sort) {
-            const sortBy = req.query.sort.split(",").join(" ");
-            query = query.sort(sortBy);
-        } else {
-            query = query.sort("-createdAt");
-        };
-
-        //4) FIELD LIMITING
-        if (req.query.fields) {
-            const fields = req.query.fields.split(",").join(" ");
-            query = query.select(fields);
-        } else {
-            query = query.select("-__v");
-        };
-
-        //5) PAGINATION
-        const page = req.query.page * 1 || 1;
-        const limit = req.query.limit * 1 || 100;
-        const skip = (page - 1) * limit;
-
-        query = query.skip(skip).limit(limit);
-
-        if (req.query.page) {
-            const numTours = await Tour.countDocuments();
-            if (skip >= numTours) throw new Error("This page does not exist");
-        }
-
-        
         //EXECUTE QUERY
-        const tours = await query;
+        const features = new APIFeatures(Tour.find(), req.query).filter().sort().limitFields().paginate();
+        const tours = await features.query;
 
         // SEND RESPONSE
         res.status(200).json({
