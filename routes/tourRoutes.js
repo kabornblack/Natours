122598eafede1217531bf6657364f1ea7aa40c6b
kabornblack <shaibu.kabiru@email.com>
2023-06-08 13:34:19 +0300

    Created route for the Aggregation pipeline Unwind and project

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -12,6 +12,7 @@ const router = express.Router();
 router.route("/top-5-cheap").get(tourController.aliasTopTours, tourController.getAllTours);
 
 router.route("/tour-stats").get(tourController.getTourStats);
+router.route("/tour-monthly-plan/:year").get(tourController.getMonthlyPlan);
 
 router.route("/").get(tourController.getAllTours).post(tourController.createTour);
 router.route("/:id").get(tourController.getTour).patch(tourController.updateTour).delete(tourController.deleteTour);
