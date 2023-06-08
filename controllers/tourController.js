107218726f7cadc8cb2ee36b30470b125d3882fc
kabornblack <shaibu.kabiru@email.com>
2023-06-08 13:33:01 +0300

    Set aggregation pipeline 'unwind and project'

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -152,4 +152,57 @@ exports.getTourStats = async (req, res) => {
     }
 };
 
+exports.getMonthlyPlan = async (req, res) => {
+    try {
+        const year = req.params.year * 1; // 2021
+
+        const plan = await Tour.aggregate([
+            {
+                $unwind: "$startDates"
+            },
+            {
+                $match: {
+                    startDates: { 
+                        $gte: new Date(`${year}-01-01`),
+                        $lte: new Date(`${year}-12-31`)
+                    }
+                }
+            },
+            {
+                $group: {
+                    _id: { $month: "$startDates"},
+                    numTourStarts: { $sum: 1 },
+                    tours: { $push: "$name" }
+                }
+            },
+            {
+                $addFields: { month: "$_id" } //To get the month written
+            },
+            {
+                $project: { 
+                    _id: 0  // To hide the _id
+                }
+            },
+            {
+                $sort: { numTourStarts: -1}
+            },
+            {
+                $limit: 12
+            }
+        ]);
+
+        res.status(200).json({
+            status: "success",
+            data: {
+             plan
+            }
+        });
+    } catch (err) {
+        res.status(404).json({
+            status: "fail",
+            message: err
+        });
+    }
+};
+
 
