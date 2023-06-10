b314c78779f58e786226d4d53e29935a6bab49b2
kabornblack <shaibu.kabiru@email.com>
2023-06-11 00:23:14 +0300

    Implemented Global error handler and route error handler

--- app.js
+++ app.js
@@ -1,33 +1,25 @@
 const express = require("express");
 const morgan = require("morgan");
 
-
 const tourRouter = require("./routes/tourRoutes");
 const userRouter = require("./routes/userRoutes");
 
 const app = express();
 
-
-
 // 1) MIDDLEWARES
 
-app.use(morgan("dev"));
+
+if (process.env.NODE_ENV === "development") {
+    app.use(morgan("dev"));
+};
+
 app.use(express.json());
 app.use(express.static(`${__dirname}/starter/public`));
-
-app.use((req, res, next) => {
-    console.log("Hello from the middleware");
-    next();
-});
 
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
     next();
 });
-
-
-
-
 
 // 2) ROUTES
 
@@ -37,10 +29,32 @@ app.use((req, res, next) => {
 // app.patch("/api/v1/tours/:id", updateTour);
 // app.delete("/api/v1/tours/:id", deleteTour);
 
-
-
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
 
+//Route error handling
+app.all("*", (req, res, next) => {
+    // res.status(404).json({
+    //     status: "fail",
+    //     message: `Cant find ${req.originalUrl} on this server!`
+    // })
+
+    const err = new Error(`Cant find ${req.originalUrl} on this server!`);
+    err.status = "fail";
+    err.statusCode = 404;
+
+    next(err);
+});
+
+//Global error handler
+app.use((err, req, res, next) => {
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || "error";
+
+    res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message
+    });
+});
 
 module.exports = app;
