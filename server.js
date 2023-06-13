dc8bff5a918dadeee9c972eff7f8fda9ca85d041
kabornblack <shaibu.kabiru@email.com>
2023-06-13 21:16:35 +0300

    Hanldled UNHANDLED REJECTION

--- server.js
+++ server.js
@@ -1,53 +1,28 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const app = require("./app");
 
 dotenv.config({path: "./config.env"});
+const app = require("./app");
+
+
 
 const DB = process.env.DATABASE.replace("<PASSWORD>", process.env.DATABASE_PASSWORD);
 
 mongoose.connect(DB, {
     //to connect to local (process.env.DATABASE_LOCAL, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-  }).then(con => {
-    //con is decleared but never used
-    console.log("DB connection successfull!");
-});
+    useUnifiedTopology: true,
+  }).then(() => console.log("DB connection successfull!")); //.catch(err => console.log("ERROR")); //To handle unhandled promise rejection
 
-const tourSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, "A tour must have a name"],
-        unique: true
-    },
-    rating: {
-        type: Number,
-        default: 4.5
-    },
-    price: {
-        type: Number,
-        required: [true, "A tour must have a price"]
-    } 
-});
-
-const Tour = mongoose.model("Tour", tourSchema);
-
-const testTour = new Tour({
-    name: "The forest hiker",
-    rating: 4.7,
-    price: 499
-});
-
-testTour.save().then(doc => {
-    console.log(doc);
-}).catch(err => {
-    console.log("ERROR saving!", err);
-});
-
-
-const port = process.env.port;
-app.listen(port, () => {
+const port = process.env.PORT;
+const server = app.listen(port, () => {
     console.log(`Server listening on port ${port}...`);
+});
 
+process.on("unhandledRejection", err => {
+  console.log(err.name, err.message);
+  console.log("UNHANDLED REJECTION!. App shutting down...");
+  server.close(() => {
+    process.exit(1);
+  })
 });
