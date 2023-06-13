fd8e4ed3b7c894268b2d440db03a6ce1e1ea12ea
kabornblack <shaibu.kabiru@email.com>
2023-06-13 20:45:35 +0300

    Handled mongoose error: Validation error, Duplicate error and Cast error

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,25 @@
+const AppError = require("./../utils/appError");
+
+const handleCastErrorDB = err => {
+    const message = `Invalid ${err.path}: ${err.value}.`;
+    return new AppError(message, 400);
+};
+
+const handleDuplicateFieldDB = err => {
+
+    const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+    console.log(value);
+
+    const message = `Duplicate field value: ${value}. Please use another value`;
+    return new AppError(message, 404);
+};
+
+const handleValidatorErrorDB = err => {
+    const errors = Object.values(err.errors).map(el => el.message);
+
+    const message = `Invalid input data. ${errors.join(". ")}`;
+    return new AppError(message, 400);
+};
 
 const sendErrorDev = (err, res) => {
     res.status(err.statusCode).json({
@@ -15,23 +37,36 @@ const sendErrorProd = (err, res) => {
             status: err.status,
             message: err.message,
         });
+
+        //Programming or other unknown errors: Dont leak details to client
     } else {
+        //1) Log error
+        console.error("ERROR", err);
+
+        //2) Send generic message
         res.status(500).json({
             status: "error",
             message: "Something went very wrong!"
         })
     }
 
-}
+};
 
 module.exports = (err, req, res, next) => {
 
     err.statusCode = err.statusCode || 500;
     err.status = err.status || "error";
 
-    if (process.env.NODE_ENV === "developement") {
+    if (process.env.NODE_ENV === "development") {
             sendErrorDev(err, res)
         } else if (process.env.NODE_ENV === "production") {
-            sendErrorProd(err, res)
+            let error = { ...err };
+
+            if (err.name === "CastError") error = handleCastErrorDB(error);
+            if (error.code === 11000) error = handleDuplicateFieldDB(error);
+            if (error.name === ValidatorError) error.handleValidatorErrorDB(error);
+
+
+            sendErrorProd(error, res);
     } 
 }
