846ba7e7306cb879e078426f7f50f6d57e1bf2a6
kabornblack <shaibu.kabiru@email.com>
2023-06-14 23:02:46 +0300

    Hashed password to provent hackers accessing it or any other brute force attack

---
+++ models/userModel.js
@@ -0,0 +1,60 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Please tell us your name!"]
+    },
+    email: {
+        type: String,
+        required: [true, "Please provide your email address"],
+        unique: [true, "Email address already in use"],
+        lowercase: true,
+        validate: [validator.isEmail, "Please provide a valid email address"]
+        // {
+        //     validator: function (value) {
+        //       // Regular expression for email validation
+        //       const emailRegex = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
+        //       return emailRegex.test(value);
+        //     },
+        //     message: "Please enter a valid email address",
+        //   },
+    },
+    photo: {
+        type: String,
+    },
+    password: {
+        type: String,
+        required: [true, "Please provide a password"],
+        minlength: 8
+    },
+    passwordConfirmed: {
+        type: String,
+        required: [true, "Please confirm your password"],
+        validate: {
+            //This is only going to work on CREATE and SAVE!!!
+            validator: function (value) {
+            return value === this.password;
+            },
+            message: "Passwords do not match",
+        }
+    }
+});
+
+userSchema.pre("save", async function(next) {
+    //Only run this if password was modified
+    if (!this.isModified("password")) return next();
+
+    //Hash the password with cost of 12
+    this.password = await bcrypt.hash(this.password, 12);
+
+    //Delete password confirmed field
+    this.passwordConfirmed = undefined;
+    next();
+})
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
