b19df4ea4d46ab0c297b91463ac36a7b9df3168c
kabornblack <shaibu.kabiru@email.com>
2023-06-15 22:12:13 +0300

    Signing Up users and creating web token with JWT

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,4 @@
+const jwt = require("jsonwebtoken");
 const User = require("./../models/userModel");
 const catchAsync = require("./../utils/catchAsync");
 
@@ -5,8 +6,22 @@ const catchAsync = require("./../utils/catchAsync");
 exports.signup = catchAsync(async (req, res, next) => {
     const newUser = await User.create(req.body);
 
+    //THIS WILL BE THE BEST PRACTICE TO SIGN UP USER AS NORMAL AND THEN WE CAN CADD ADMIN ROLE ON MONGODB IF THEY ARE TO BE SIGNED AS ADMIN
+    // const newUser = await User.create({
+    //     name: req.body.name,
+    //     email: req.body.email,
+    //     password: req.body.password,
+    //     passwordConfirmed: req.body.passwordConfirmed
+    // });
+
+    const expiresIn = 3600;
+    const token = jwt.sign({ id: newUser._id}, process.env.JWT_SECRET, {
+        expiresIn: expiresIn
+    });
+
     res.status(201).json({
         status: "successfull",
+        token,
         data: {
             user: newUser
         }
