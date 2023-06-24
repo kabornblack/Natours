6e7e2e684d3e81b3720c21da0467f9e9cab2ef3b
kabornblack <shaibu.kabiru@email.com>
2023-06-24 10:35:23 +0300

    Protected tour routes

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,10 @@
+const { promisify } = require("util");
 const jwt = require("jsonwebtoken");
 const User = require("./../models/userModel");
 const catchAsync = require("./../utils/catchAsync");
 const AppError = require("./../utils/appError");
 
-const expiresIn = 3600;
+const expiresIn = 9111100;
 
 const signToken = id => {
     return jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -60,4 +61,34 @@ exports.login = catchAsync( async (req, res, next) => {
         status: "successful",
         token
     });
+});
+
+exports.protect = catchAsync ( async (req, res, next) => {
+    //1) Get the token and check it it exist
+    let token;
+    if (req.headers.authorization && req.headers.authorization.startsWith("Bearer"))
+     {token = req.headers.authorization.split(" ")[1];}
+
+    if (!token) {
+        return next(new AppError("You are not logged in! Please log in to get access", 401));
+    }
+
+    //2) Varification token
+    const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+
+    //3)Check if user still exist
+    const freshUser = await User.findById(decoded.id);
+    if (!freshUser) {
+        return next(new AppError("The user belonging to this token does no longer exist.", 401));
+    }
+
+
+    //4) Check if user change password after the JWT was issued
+    if(freshUser.changePasswordAfter(decoded.iat)) {
+        return next(new AppError("User recently changed password! please log in again", 401));
+    };
+
+    //GRANT ACCESS TO PROTECTED ROUTE
+    req.user = freshUser;
+    next();
 });
